f478d826cd2cf7bda8024cb2496742f07fdbbe80
MicheleIanni86 <micheleianni86@gmail.com>
2024-05-08 16:05:31 +0200

    feat: Add Structure Search Button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,9 +16,12 @@ export default {
 
     <div class="container-main">
 
+        <div class="blood-button">
+            Cerca
+        </div>
+
 
     </div>
-
 
 </template>
 
@@ -30,7 +33,43 @@ export default {
     background-image: url(/src/img/background-home.jpg);
     background-position: center;
     background-size: cover;
+    position: relative;
 }
+
+.blood-button {
+    text-align: center;
+    display: inline-block;
+    width: 120px;
+    /* Larghezza del pulsante */
+    height: 200px;
+    /* Altezza del pulsante */
+    background-color: red;
+    background-image: radial-gradient(circle at 50% 30%, white, transparent 25%), linear-gradient(to bottom, #bb0000, #880000);
+    border-radius: 25px;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+
+
+}
+
+
+
+.blood-button:hover {
+    background-image: radial-gradient(circle at 50% 40%, white, transparent 30%), linear-gradient(to bottom, #cc0000, #aa0000);
+}
+
+
+
+
+
 
 ;
 </style>
