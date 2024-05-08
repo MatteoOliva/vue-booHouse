205be5400ed11c7c8b564e1d1c3ec58b7ee220cd
MicheleIanni86 <micheleianni86@gmail.com>
2024-05-08 11:14:26 +0200

    feat: Added Fontawesone

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,8 @@ import axios from "axios";
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>WEEEE <font-awesome-icon icon="fa-solid fa-user" /></h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import { createApp } from 'vue';
 import App from './App.vue';
 
-import * as bootstrap from bootstrap;
+import * as bootstrap from "bootstrap";
 
+// / import the fontawesome core /
+import { library } from '@fortawesome/fontawesome-svg-core';
+// / import font awesome icon component /
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// / import specific icons  from the 3 free kits/
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons';
+import { } from '@fortawesome/free-brands-svg-icons';
+// / add icons to the library */
+library.add(faUser);
+
+
+import { createApp } from 'vue';
 const app = createApp(App);
-app.mount(App).mount('#app');
+
+app
+    .component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
