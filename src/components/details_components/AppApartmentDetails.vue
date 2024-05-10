0d1cf3e4064167d4f60047bac18cff7d1949ccd9
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-05-10 18:04:08 +0200

    feat: add AppApartmentDetails

---
+++ src/components/details_components/AppApartmentDetails.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+import { store, api } from "../../store";
+
+export default {
+  props: { apartment: Object },
+
+  data() {
+    return {
+
+    };
+  },
+
+  components: {  },
+};
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>{{ apartment.title }}</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/AppDetails.vue
+++ src/pages/AppDetails.vue
@@ -1,5 +1,6 @@
 <script>
 import AppMain from "../components/AppMain.vue";
+import AppApartmentDetails from "../components/details_components/AppApartmentDetails.vue";
 import axios from "axios";
 import { store, api } from "../store";
 
@@ -9,13 +10,27 @@ export default {
 
     };
   },
+
+  components: { AppMain, AppApartmentDetails },
 };
+
 </script>
 
 <template>
-
+    <div class="container-app-details">
+        <AppApartmentDetails />
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+.container-app-details {
+    height: calc(100vh - 170px);
+    flex-grow: 1;
+    background-color: gray;
+    background-image: url(/src/img/background-search.jpg);
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    overflow: auto;
+    }
 </style>
