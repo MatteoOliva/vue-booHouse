9ff0a173c9379cc7b2349622bebdaaf01043a41b
MicheleIanni86 <micheleianni86@gmail.com>
2024-05-12 18:46:26 +0200

    feat: Adjust Card Details

--- src/components/details_components/AppApartmentDetails.vue
+++ src/components/details_components/AppApartmentDetails.vue
@@ -19,24 +19,113 @@ export default {
       :to="{ name: 'project', params: { slug: project.slug } }"
       class="card-btn"
     > -->
-  <!-- <div class="card h-100"> -->
-  <div class="cover">
-    <img v-if="apartment.image" :src="apartment.image" class="card-img-top" />
-  </div>
-  <h5>{{ apartment }}</h5>
-  <!-- <div class="card-body">
-        <h5 class="card-title">{{ apartment.title }}</h5>
-        <p>{{ apartment.address }}</p>
+  <div class="container-card">
+
+    <div class="card main-card">
+
+      <div class="row">
+
+        <div class="col-6 text-center mt-4">
+          <h2>Alloggio: <strong>{{ apartment.title }}</strong></h2>
+
+          <div class="row">
+            <h4 class="col-12">{{ apartment.address }}</h4>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-6">
+              <h5>Metri Quadri: {{ apartment.mq }}</h5>
+            </div>
+            <div class="col-6">
+              <h5>Camere: {{ apartment.rooms }}</h5>
+            </div>
+          </div>
+
+          <div class="row mt-4">
+            <div class="col-6">
+              <h5>Letti: {{ apartment.beds }}</h5>
+            </div>
+            <div class="col-6">
+              <h5>Bagni: {{ apartment.toilets }}</h5>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-6">
+
+          <div class="container-main-img">
+            <img v-if="apartment.image" :src="apartment.image" class="" />
+          </div>
+
+        </div>
+
       </div>
-    </div> -->
+
+
+      <div class="row">
+
+        <div class="col-12 mt-4 text-center">
+          <h5>{{ apartment.description }}</h5>
+        </div>
+
+      </div>
+
+      <div>
+        <message-button v-if="apartment" :apartment_id="this.apartment.id"></message-button>
+      </div>
+
+    </div>
+
+  </div>
+  {{ apartment }}
+
+
+  <!-- <div class="card-body">
+    <h5 class="card-title">{{ apartment.title }}</h5>
+    <p>{{ apartment.address }}</p>
+  </div>
+</div> -->
   <!-- </router-link> -->
   <!-- </div> -->
 
-  <!-- BOTTONE INVIA MESSAGGIO -->
-  <message-button
-    v-if="apartment"
-    :apartment_id="this.apartment.id"
-  ></message-button>
+
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  border: 2px dashed red;
+
+  .main-card {
+
+
+    padding: 3%;
+    width: 90%;
+    height: 90%;
+    box-shadow: 15px 15px 15px rgba($color: #000000, $alpha: 0.7);
+
+    .container-main-img {
+      border-radius: 15px;
+      height: 300px;
+      overflow: hidden;
+
+
+
+
+      img {
+        width: 100%;
+        max-height: 100%;
+        object-fit: cover;
+        object-position: center;
+      }
+
+    }
+
+  }
+}
+</style>
--- src/components/details_components/MessageButton.vue
+++ src/components/details_components/MessageButton.vue
@@ -58,35 +58,19 @@ export default {
 
 <template>
   <!-- <h5>PROVA BOTTONE</h5> -->
-  <button
-    type="button"
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-    data-bs-whatever="@mdo"
-  >
-    Scrivi messaggio
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+    data-bs-whatever="@mdo">
+    Contatta Alloggio
   </button>
 
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Scrivi a {{ this.apartment_id }}
           </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <form method="post" @submit.prevent="createMessage">
@@ -98,33 +82,15 @@ export default {
             /> -->
             <div class="mb-3">
               <label for="email" class="col-form-label">E-Mail:</label>
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                v-model="email"
-                ref="email"
-                autocomplete="email"
-                required
-              />
+              <input type="email" class="form-control" id="email" v-model="email" ref="email" autocomplete="email"
+                required />
             </div>
             <div class="mb-3">
               <label for="content" class="col-form-label">Messaggio:</label>
-              <textarea
-                class="form-control"
-                id="content"
-                rows="5"
-                ref="content"
-                v-model="content"
-                required
-              ></textarea>
+              <textarea class="form-control" id="content" rows="5" ref="content" v-model="content" required></textarea>
             </div>
             <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                 Chiudi
               </button>
               <button type="submit" class="btn btn-primary">Invia</button>
