aba818487b3d244d5b7f78ec888d9b4326eb9d0a
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-05-13 19:28:56 +0200

    feat: aggiunta  carosello nel main

--- src/components/search_components/CarouselApartments.vue
+++ src/components/search_components/CarouselApartments.vue
@@ -1,13 +1,14 @@
 <script>
-import axios from "axios";
-
+// import axios from "axios";
 // importo store
 import { store, api } from "../../store";
-
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 // importo card
 // import CardApartment from "./CardApartment.vue";
 
-export default {
+export default defineComponent({
   data() {
     return {
       store,
@@ -15,19 +16,21 @@ export default {
     };
   },
 
-  // components: { CardApartment },
+  components: { Carousel, Slide, Pagination},
+
   created() {
     store.fetchApartmentsSponsor();
   },
-};
+  methods:{
+ 
+  },
+
+});
 </script>
 
 <template>
-  <div class="container-carousel">
+  <!-- <div class="container-carousel">
 
-    <div class="text-center title-ev">
-      <div>Alloggi Infestati in evidenza...</div>
-    </div>
     < <div id="carouselExampleInterval" class="carousel slide carosello" data-bs-ride="carousel">
       <div class="carousel-inner" style="height: 400px; border-radius: 50px;">
         <div class="carousel-item" :class="index == 0 ? 'active' : ''" data-bs-interval="2000"
@@ -40,36 +43,83 @@ export default {
 
   </div>
 
+  </div> -->
+  <div class="text-center title-ev">
+      <div>Alloggi Infestati in evidenza...</div>
   </div>
-
+  <Carousel :autoplay="2500" :itemsToShow="3.95" :wrapAround="true" :transition="500" >
+    <Slide v-for="(apartment, index) in store.sponsoredApartments.sponsored_apartments" :key="index">
+      <div class="carousel__item">
+        <router-link :to="{ name: 'details', params: { slug: apartment.slug } }">
+          <img :src="apartment.image" class="d-block w-100" style="height: 180px; width: auto; object-fit: cover; border-radius: 20px;" :alt="apartment.title">
+            <h5 class="titoloslider">{{ apartment.title }}</h5>
+        </router-link>
+      </div>
+    </Slide> 
+  </Carousel>
 </template>
 
 <style lang="scss" scoped>
-.container-carousel {
-  position: relative;
-
-  .title-ev {
-    color: white;
-    font-size: 20px;
-    text-align: center;
-    font-weight: bold;
-    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
-      1px 1px 0 #000;
-    z-index: 1;
-
-    position: absolute;
-    top: 30px;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+.title-ev {
+  color: white;
+  font-size: 20px;
+  text-align: center;
+  font-weight: bold;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+    1px 1px 0 #000;
+  z-index: 1;
+  position: absolute;
+  top: -50px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 
-.carosello {
+.titoloslider{
+  text-decoration: none !important;
+  color: white ;
+  font-size: 15px ;
+  text-align: center ;
+  font-weight: bold ;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000 ;
+}
 
-  height: 100px;
+.carousel__slide {
+  padding: 5px;
+}
 
-  img: {
-    // max-height: 100%;
-  }
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
 }
 </style>
