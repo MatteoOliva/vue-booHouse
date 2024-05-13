d50d8762e4d2e96cbe44650c2fbfb0e4b0b426db
lorenzodisanto <lore.disanto95@gmail.com>
2024-05-13 09:52:34 +0200

    fix: change props

--- src/components/details_components/MessageButton.vue
+++ src/components/details_components/MessageButton.vue
@@ -18,14 +18,14 @@ export default {
   },
 
   props: {
-    apartment_id: Number,
+    apartment: Object,
   },
 
   methods: {
     createMessage() {
       // salvo dati del form
       let formData = {
-        apartment_id: this.apartment_id,
+        apartment_id: this.apartment.id,
         email: document.getElementById("email").value,
         content: document.getElementById("content").value,
       };
@@ -58,19 +58,35 @@ export default {
 
 <template>
   <!-- <h5>PROVA BOTTONE</h5> -->
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
-    data-bs-whatever="@mdo">
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+    data-bs-whatever="@mdo"
+  >
     Contatta Alloggio
   </button>
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
-      <div class="modal-content  bg-dark">
+      <div class="modal-content bg-dark">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
-            Scrivi a {{ this.apartment_id }}
+            Scrivi a {{ this.apartment.title }}
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body">
           <form method="post" @submit.prevent="createMessage">
@@ -82,15 +98,33 @@ export default {
             /> -->
             <div class="mb-3">
               <label for="email" class="col-form-label">E-Mail:</label>
-              <input type="email" class="form-control" id="email" v-model="email" ref="email" autocomplete="email"
-                required />
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                v-model="email"
+                ref="email"
+                autocomplete="email"
+                required
+              />
             </div>
             <div class="mb-3">
               <label for="content" class="col-form-label">Messaggio:</label>
-              <textarea class="form-control" id="content" rows="20" ref="content" v-model="content" required></textarea>
+              <textarea
+                class="form-control"
+                id="content"
+                rows="20"
+                ref="content"
+                v-model="content"
+                required
+              ></textarea>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
                 Chiudi
               </button>
               <button type="submit" class="btn btn-primary">Invia</button>
