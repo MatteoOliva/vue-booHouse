1747bc908d6d263624f171fe8057e69620d36e9b
MicheleIanni86 <micheleianni86@gmail.com>
2024-05-14 10:13:35 +0200

    feat: Add Skull

--- src/components/details_components/MessageButton.vue
+++ src/components/details_components/MessageButton.vue
@@ -47,17 +47,24 @@ export default {
         // se la risposta dal db è positiva
         if (response.data.response) {
           Swal.fire({
-            position: "top-end",
+            position: "center",
+            width: 600,
             icon: "success",
-            title: "Il messaggio è stato inviato",
+            title: "Il messaggio è stato inviato alla struttura",
             showConfirmButton: false,
-            timer: 4500,
+
+
+            backdrop: ` 
+    rgba(0,0,0,0.4)
+    url("/skull-gif.gif")
+    left center
+    no-repeat`
           });
-          // alert("messaggio inviato con successo");
+
 
           setTimeout(() => {
             window.location.reload();
-          }, 2000);
+          }, 4000);
         }
         // this.resetForm();
       });
@@ -88,71 +95,35 @@ export default {
 
 <template>
   <!-- <h5>PROVA BOTTONE</h5> -->
-  <button
-    type="button"
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-    data-bs-whatever="@mdo"
-  >
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+    data-bs-whatever="@mdo">
     Contatta Alloggio
   </button>
 
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content bg-dark">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Scrivi a {{ this.apartment.title }}
           </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <form id="messageform" method="post" @submit.prevent="createMessage">
             <div class="mb-3">
               <label for="email" class="col-form-label">E-Mail:</label>
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                ref="email"
-                autocomplete="email"
-                v-model="email"
-                required
-              />
+              <input type="email" class="form-control" id="email" ref="email" autocomplete="email" v-model="email"
+                required />
               <span v-if="!isEmailValid">Inserisci mail</span>
             </div>
             <div class="mb-3">
               <label for="content" class="col-form-label">Messaggio:</label>
-              <textarea
-                class="form-control"
-                id="content"
-                rows="20"
-                ref="content"
-                v-model="content"
-                required
-              ></textarea>
-              <span v-if="!isContentValid"
-                >Il contenuto deve essere di almeno 10 caratteri.</span
-              >
+              <textarea class="form-control" id="content" rows="20" ref="content" v-model="content" required></textarea>
+              <span v-if="!isContentValid">Il contenuto deve essere di almeno 10 caratteri.</span>
             </div>
             <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                 Chiudi
               </button>
               <button type="submit" class="btn btn-primary">Invia</button>
