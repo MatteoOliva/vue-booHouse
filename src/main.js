76de2ee36cae66a569e4716c2f05d964d8a744bd
MatteoOliva <oliva.matteo98@outlook.com>
2024-05-15 18:41:58 +0200

    feat: sponsor star and border

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 // / import font awesome icon component /
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 // / import specific icons  from the 3 free kits/
-import { faUser, faSkull, faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSkull, faMagnifyingGlass, faLocationDot, faStar } from '@fortawesome/free-solid-svg-icons';
 import { } from '@fortawesome/free-regular-svg-icons';
 import { } from '@fortawesome/free-brands-svg-icons';
 // / add icons to the library */
-library.add(faUser, faSkull, faMagnifyingGlass, faLocationDot);
+library.add(faUser, faSkull, faMagnifyingGlass, faLocationDot, faStar);
 
 
 import { createApp } from 'vue';
