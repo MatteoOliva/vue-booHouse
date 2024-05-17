0f658fd0a2fbd885c541ea363b7038747090d0f1
SerenaMalusa <sere.malu@gmail.com>
2024-05-17 15:23:24 +0200

    feat: creati i badge per la visione dei filtri della ricerca avanzata

--- src/main.js
+++ src/main.js
@@ -1,33 +1,55 @@
-import App from './App.vue';
+import App from "./App.vue";
 
-import { router } from './router';
-
+import { router } from "./router";
 
 import * as bootstrap from "bootstrap";
 
 // / import the fontawesome core /
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from "@fortawesome/fontawesome-svg-core";
 // / import font awesome icon component /
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 // / import specific icons  from the 3 free kits/
 
-import { faUser, faSkull, faMagnifyingGlass, faLocationDot, faEnvelope, faArrowLeft, faStar, faMap, faPersonShelter, faBed, faBath, faDownLong, faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope as faEnvelopeRegular } from '@fortawesome/free-regular-svg-icons';
-import { } from '@fortawesome/free-brands-svg-icons';
+import {
+  faXmark,
+  faUser,
+  faSkull,
+  faMagnifyingGlass,
+  faLocationDot,
+  faEnvelope,
+  faArrowLeft,
+  faStar,
+  faMap,
+  faPersonShelter,
+  faBed,
+  faBath,
+  faDownLong,
+  faSkullCrossbones,
+} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope as faEnvelopeRegular } from "@fortawesome/free-regular-svg-icons";
+import {} from "@fortawesome/free-brands-svg-icons";
 // / add icons to the library */
-library.add(faUser, faSkull, faMagnifyingGlass, faLocationDot, faEnvelope, faEnvelopeRegular, faArrowLeft, faStar, faMap, faPersonShelter, faBed, faBath, faDownLong, faSkullCrossbones);
+library.add(
+  faXmark,
+  faUser,
+  faSkull,
+  faMagnifyingGlass,
+  faLocationDot,
+  faEnvelope,
+  faEnvelopeRegular,
+  faArrowLeft,
+  faStar,
+  faMap,
+  faPersonShelter,
+  faBed,
+  faBath,
+  faDownLong,
+  faSkullCrossbones
+);
 
-
-
-
-
-
-
-
-
-import { createApp } from 'vue';
+import { createApp } from "vue";
 const app = createApp(App);
 
-app.component('font-awesome-icon', FontAwesomeIcon);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
